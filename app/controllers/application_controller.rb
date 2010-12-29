39b375430bd5ae1c77c60180bede6d7034651609
Rafal <rafalx89@gmail.com>
2010-12-30 00:01:53 +0100

    added home controller and layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  layout "home"
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
